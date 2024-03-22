cc49f2c985a8b9da055795f775400c24a285e64d
Luis Burgos <lburgos@nationalnotary.org>
2024-03-22 14:56:04 -0400

    Employee

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   company_id: '65fcc47e6d31ec8c5a8bce48',
   //base_url: 'http://localhost:3000/api'
-  base_url: 'https://app-rentacar-82bb0263d673.herokuapp.com'
+  base_url: 'https://app-rentacar-82bb0263d673.herokuapp.com/api'
 };
